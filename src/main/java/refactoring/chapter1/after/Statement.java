84aeecd051525c27ca73ce1493da161dfc5a5376
justin.park <justin.ad.park@gmail.com>
2025-02-13 09:44:00 +0900

    ch01-06 slide statements 변수 선언(초기화)을 반복문 앞으로 이동

--- src/main/java/refactoring/chapter1/after/Statement.java
+++ src/main/java/refactoring/chapter1/after/Statement.java
@@ -13,7 +13,6 @@ public class Statement {
 
     public String statement(Invoice invoice, Map<String, Play> plays) {
         var totalAmount = 0;
-        var volumeCredits = 0;
         var result = new StringBuilder("청구 내역 (고객명: " + invoice.customer() + ")\n");
 
         for (var perf : invoice.performances()) {
@@ -31,6 +30,7 @@ public class Statement {
             totalAmount += amountFor(perf, play);
         }
 
+        var volumeCredits = 0;  // <- 변수 선언을 반복문 앞으로 이동, Slide Statements(리팩터링 8.6 문장 슬라이드하기)
         for (var perf : invoice.performances()) {   // <- 값 누적 로직을 별도 for문으로 분리
             final Play play = plays.get(perf.playID());
 
