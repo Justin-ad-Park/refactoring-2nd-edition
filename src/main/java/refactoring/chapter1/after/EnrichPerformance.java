491557fc0f5df59527c560d528c826f536aa7bf0
justin.park <justin.ad.park@gmail.com>
2025-02-14 11:21:22 +0900

    ch01-17 move method to OOP class

--- src/main/java/refactoring/chapter1/after/EnrichPerformance.java
+++ src/main/java/refactoring/chapter1/after/EnrichPerformance.java
@@ -21,4 +21,27 @@ public record EnrichPerformance(
     private static Play playFor(Performance aPerformance, Map<String, Play> plays) {
         return plays.get(aPerformance.playID());
     }
+
+    public int amountFor() {
+        int result;
+
+        switch (play().type()) {
+            case "tragedy":
+                result = 40000;
+                if (audience() > 30) {
+                    result += 1000 * (audience() - 30);
+                }
+                break;
+            case "comedy":
+                result = 30000;
+                if (audience() > 20) {
+                    result += 10000 + 500 * (audience() - 20);
+                }
+                result += 300 * audience();
+                break;
+            default:
+                throw new IllegalArgumentException("알 수 없는 장르: " + play().type());
+        }
+        return result;
+    }
 }
--- src/main/java/refactoring/chapter1/after/Statement.java
+++ src/main/java/refactoring/chapter1/after/Statement.java
@@ -1,7 +1,6 @@
 package refactoring.chapter1.after;
 
 import refactoring.chapter1.data.Invoice;
-import refactoring.chapter1.data.Performance;
 import refactoring.chapter1.data.Play;
 
 import java.text.NumberFormat;
@@ -15,10 +14,10 @@ public class Statement {
     public String statement(Invoice invoice, Map<String, Play> plays) {
         StatementVo statementVo = new StatementVo(invoice, plays);
 
-        return renderPlainText(statementVo, plays);
+        return renderPlainText(statementVo);
     }
 
-    private static String renderPlainText(StatementVo statementVo, Map<String, Play> plays) {
+    private static String renderPlainText(StatementVo statementVo) {
         var result = new StringBuilder("청구 내역 (고객명: " + statementVo.customer() + ")\n");
 
         for (var perf : statementVo.enrichPerformances()) {
@@ -28,34 +27,31 @@ public class Statement {
                     String.format(
                             "  %s: %s원 (%d석)\n",
                             perf.play().name(),
-                            formatKRW(amountFor(perf) / 100.0),
+                            formatKRW(perf.amountFor() / 100.0),
                             perf.audience()
                     )
             );
         }
 
-        result.append(String.format("총액: %s원\n", formatKRW(totalAmount(statementVo.enrichPerformances(), plays) / 100.0)));
-        result.append(String.format("적립 포인트: %d점\n", totalVolumeCredits(statementVo.performances(), plays)));
+        result.append(String.format("총액: %s원\n", formatKRW(totalAmount(statementVo.enrichPerformances()) / 100.0)));
+        result.append(String.format("적립 포인트: %d점\n", totalVolumeCredits(statementVo.enrichPerformances())));
 
         return result.toString();
     }
 
-    private static int totalAmount(List<EnrichPerformance> performances, Map<String, Play> plays) {
+    private static int totalAmount(List<EnrichPerformance> performances) {
         var result = 0;
         for (var perf : performances) {
-            final Play play = plays.get(perf.playID());
 
-            result += amountFor(perf);
+            result += perf.amountFor();
         }
         return result;
     }
 
-    private static int totalVolumeCredits(List<Performance> performances, Map<String, Play> plays) {
+    private static int totalVolumeCredits(List<EnrichPerformance> enrichPerformances) {
         var result = 0;
-        for (var perf : performances) {
-            final Play play = plays.get(perf.playID());
-
-            result += volumeCreditsFor(perf, play);
+        for (var perf : enrichPerformances) {
+            result += volumeCreditsFor(perf);
         }
         return result;
     }
@@ -64,38 +60,16 @@ public class Statement {
         return NumberFormat.getCurrencyInstance(Locale.US).format(number);
     }
 
-    private static int volumeCreditsFor(Performance aPerformance,  Play play) {
+    private static int volumeCreditsFor(EnrichPerformance enrichPerformance) {
         int result = 0;
-        result += Math.max(aPerformance.audience() - 30, 0);
+        result += Math.max(enrichPerformance.audience() - 30, 0);
 
         // 희극 관객 5명마다 추가 포인트를 제공한다.
-        if ("comedy".equals(play.type())) {
-            result += Math.floor(aPerformance.audience() / 5);
+        if ("comedy".equals(enrichPerformance.play().type())) {
+            result += Math.floor(enrichPerformance.audience() / 5);
         }
         return result;
     }
 
-    private static int amountFor(EnrichPerformance aPerformance) {
-        int result;
-
-        switch (aPerformance.play().type()) {
-            case "tragedy":
-                result = 40000;
-                if (aPerformance.audience() > 30) {
-                    result += 1000 * (aPerformance.audience() - 30);
-                }
-                break;
-            case "comedy":
-                result = 30000;
-                if (aPerformance.audience() > 20) {
-                    result += 10000 + 500 * (aPerformance.audience() - 20);
-                }
-                result += 300 * aPerformance.audience();
-                break;
-            default:
-                throw new IllegalArgumentException("알 수 없는 장르: " + aPerformance.play().type());
-        }
-        return result;
-    }
 
 }
