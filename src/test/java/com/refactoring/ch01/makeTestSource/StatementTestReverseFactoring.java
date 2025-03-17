e53118f2f29a269292b92dab84f6a1a8bae87763
Justin.park <justin.ad.park@gmail.com>
2025-03-17 09:44:55 +0900

    1단계: 부모 클레스(Ch01Test) 기능 머지

--- src/test/java/com/refactoring/ch01/makeTestSource/StatementTestReverseFactoring.java
+++ src/test/java/com/refactoring/ch01/makeTestSource/StatementTestReverseFactoring.java
@@ -1,21 +1,40 @@
-package com.refactoring.ch01.before;
+package com.refactoring.ch01.makeTestSource;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.refactoring.ch01.Ch01DataLoader;
 import com.refactoring.ch01.Ch01Test;
+import com.refactoring.ch01.before.Statement;
 import com.refactoring.ch01.data.Invoice;
+import com.refactoring.ch01.data.Play;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+import java.util.Map;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
-class StatementTestReverseFactoring extends Ch01Test {
-    private Statement sut;
-    private Invoice invoice;
+class StatementTestReverseFactoring {
+    protected Map<String, Play> plays;
+    protected List<Invoice> invoices;
+
+    private static final Ch01DataLoader DATA_LOADER = new Ch01DataLoader(
+            Ch01Test.class.getClassLoader(),
+            new ObjectMapper()
+    );
 
     @BeforeEach
-    void setUp() {
+    void loadData() {
+        plays = DATA_LOADER.loadPlays();
+        invoices = DATA_LOADER.loadInvoices();
+
         sut = new Statement();
         invoice = invoices.get(0);
     }
+
+    private Statement sut;
+    private Invoice invoice;
+
 
     @Test
     void statement() {
